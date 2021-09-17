b986697e6701f6dfb08e2a509925eb5b73b17688
aratrika1996 <aratrika.mukherjee26@gmail.com>
2021-09-17 17:36:42 +0530

    Minor changes

--- digital_sig_hardhat/frontend/src/components/Verify.vue
+++ digital_sig_hardhat/frontend/src/components/Verify.vue
@@ -54,14 +54,14 @@
 }
 </style>
 
-<script>
-import ECArtifact from "../contracts/EC.json";
-import contractAddress from "../contracts/contract-address.json";
+<script lang="ts">
+const ECArtifact = require("../contracts/EC.json")
+const contractAddress = require("../contracts/contract-address.json")
 import { ethers } from 'ethers';
 import { ec as EC } from 'elliptic';
 import { BinTools, Buffer } from 'avalanche'
 import createHash from 'create-hash'
-import { BN } from 'bn.js';
+import BN from 'bn.js';
 
 export default {
   name: "Verify",
@@ -80,8 +80,8 @@ export default {
 
     // Check if Metamask is installed, if installed then check if it is connected to Avalanche Fuji Testnet
     metamaskStatus() {
-      if(window.ethereum) {
-        if(window.ethereum.chainId != '0xa869') {
+      if((window as any).ethereum) {
+        if((window as any).ethereum.chainId != '0xa869') {
           this.result = "Failed: Not connected to Avalanche Fuji Testnet via Metamask."
         }
         else{
@@ -105,33 +105,33 @@ export default {
 
     //Get the message hash with magic prefix
     hashMessage() {
-        let mBuf = Buffer.from(this.msg, 'utf8')       //creates buffer with the message
-        let msgSize = Buffer.alloc(4)                 //creates a 4 bytes buffer object
+        let mBuf: Buffer = Buffer.from(this.msg, 'utf8')       //creates buffer with the message
+        let msgSize: Buffer = Buffer.alloc(4)                 //creates a 4 bytes buffer object
         msgSize.writeUInt32BE(mBuf.length, 0)         //write a number to an instance of the Buffer class
-        let msgBuf = Buffer.from(`\x1AAvalanche Signed Message:\n${msgSize}${this.msg}`, 'utf8')   //creates message buffer with the magic prefix, message length and message
-        let hash = createHash('sha256').update(msgBuf).digest()   //hashes the message buffer with sha256
-        let hashex = hash.toString('hex')       //coverts buffer to hex
-        let hashBuff = Buffer.from(hashex, 'hex')
-        let messageHash = '0x' + hashex
+        let msgBuf: Buffer = Buffer.from(`\x1AAvalanche Signed Message:\n${msgSize}${this.msg}`, 'utf8')   //creates message buffer with the magic prefix, message length and message
+        let hash: Buffer = createHash('sha256').update(msgBuf).digest()   //hashes the message buffer with sha256
+        let hashex: string = hash.toString('hex')       //coverts buffer to hex
+        let hashBuff: Buffer = Buffer.from(hashex, 'hex')
+        let messageHash: string = '0x' + hashex
         return {hashBuff, messageHash}
     },
     
     //Split the signature into r,s and v parameters
     splitSig() {
       try{
-        let bintools = BinTools.getInstance()
-        let decodedSig = bintools.cb58Decode(this.sig)      //Decode the signature as Avalanche Wallet produces cb58 encoded signature
-        const r = new BN(bintools.copyFrom(decodedSig, 0, 32))     //split first 32 bytes to r
-        const s = new BN(bintools.copyFrom(decodedSig, 32, 64))    //split next 32 bytes to s
-        const v = bintools.copyFrom(decodedSig, 64, 65).readUIntBE(0, 1)     //split last 1 byte to v
-        const sigParam = {
+        let bintools: BinTools = BinTools.getInstance()
+        let decodedSig: Buffer = bintools.cb58Decode(this.sig)      //Decode the signature as Avalanche Wallet produces cb58 encoded signature
+        const r: BN = new BN(bintools.copyFrom(decodedSig, 0, 32))     //split first 32 bytes to r
+        const s: BN = new BN(bintools.copyFrom(decodedSig, 32, 64))    //split next 32 bytes to s
+        const v: number = bintools.copyFrom(decodedSig, 64, 65).readUIntBE(0, 1)     //split last 1 byte to v
+        const sigParam: any = {
           r: r,
           s: s,
           v: v
         }
-        let rhex = '0x' + r.toString('hex')     //converts r to hex
-        let shex = '0x' + s.toString('hex')   //converts s to hex
-        let sigHex = [rhex, shex]
+        let rhex: string = '0x' + r.toString('hex')     //converts r to hex
+        let shex: string = '0x' + s.toString('hex')   //converts s to hex
+        let sigHex: Array<string> = [rhex, shex]
         return {sigParam, sigHex}
       }
       catch{
@@ -140,35 +140,35 @@ export default {
     },
 
     //Recover public key
-    recover(msg, sig) {
-      let ec = new EC('secp256k1')
-      const pubk = ec.recoverPubKey(msg, sig, sig.v)
-      const pubkx = '0x' + pubk.x.toString('hex')     //public key x coordinate
-      const pubky = '0x' + pubk.y.toString('hex')     //public key y coordinate
-      let pubkCord = [pubkx, pubky]
-      let pubkBuff = Buffer.from(pubk.encodeCompressed())
+    recover(msg: Buffer, sig: any) {
+      let ec: EC = new EC('secp256k1')
+      const pubk: any = ec.recoverPubKey(msg, sig, sig.v)
+      const pubkx: string = '0x' + pubk.x.toString('hex')     //public key x coordinate
+      const pubky: string = '0x' + pubk.y.toString('hex')     //public key y coordinate
+      let pubkCord: Array<string> = [pubkx, pubky]
+      let pubkBuff: Buffer = Buffer.from(pubk.encodeCompressed())
       return {pubkCord, pubkBuff}
     },
 
     // Verify the signature and get the signer address from Dapp
     async verify() {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const elliptic = new ethers.Contract(contractAddress.Contract, ECArtifact.abi, provider)
+      const provider: any = new ethers.providers.Web3Provider((window as any).ethereum)
+      const elliptic: any = new ethers.Contract(contractAddress.Contract, ECArtifact.abi, provider)
       if(this.xc == '' || this.msg == '' || this.sig == '') {
         this.result = "Signature Verification Failed"
       }
       else {
         let xc = this.checkXc()
         if (xc) {
-          let message = this.hashMessage()
-          let sign = this.splitSig()
-          let publicKey = this.recover(message.hashBuff, sign.sigParam)
-          let prefix = "fuji"
-          let hrp = []    //array of unicode of prefix
+          let message: any = this.hashMessage()
+          let sign: any = this.splitSig()
+          let publicKey: any = this.recover(message.hashBuff, sign.sigParam)
+          let prefix: string = "fuji"
+          let hrp: Array<any> = []    //array of unicode of prefix
           for (var i=0; i<prefix.length; i++) {
             hrp[i] = prefix.charCodeAt(i)
           }
-          const tx = await elliptic.recoverAddress(message.messageHash, sign.sigHex, publicKey.pubkCord, publicKey.pubkBuff, this.xchain, prefix, hrp)
+          const tx: string = await elliptic.recoverAddress(message.messageHash, sign.sigHex, publicKey.pubkCord, publicKey.pubkBuff, this.xchain, prefix, hrp)
           this.result = tx
         }
         else{
